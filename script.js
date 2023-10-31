815f10a162ff4f587436df0a799c84d678a39978
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-10-31 22:18:19 +0545

    Ifelse_condition
    Js

---
+++ script.js
@@ -0,0 +1,17 @@
+// If Else condition in Js
+
+let age = 15;
+let hasvotecard = "No";
+
+if(age>=14 && hasvotecard=="Yes")
+{
+    alert("You can Vote!");
+}
+else if(age>=14 && hasvotecard=="No")
+{
+    alert("Get your Voter id");
+}
+else
+{
+    alert("You cannot Vote");
+}
